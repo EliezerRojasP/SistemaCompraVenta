2fa0eab415a9ff68e39070f518f92d1092e69df4
Eliezer Rojas <142471248+EliezerRojasP@users.noreply.github.com>
2025-06-22 14:21:20 -0500

    Azure2

--- view/home/index.php
+++ view/home/index.php
@@ -75,7 +75,7 @@
                                         <div class="d-flex align-items-lg-center flex-lg-row flex-column">
                                             <div class="flex-grow-1">
                                                 <h4 class="fs-16 mb-1">Buen dia, <?php echo $_SESSION["USU_NOM"]?>!</h4>
-                                                <p class="text-muted mb-0">Here's what's happening with your store today.</p>
+                                                <p class="text-muted mb-0">Esto es lo que está pasando con tu tienda hoy.</p>
                                             </div>
                                             <div class="mt-3 mt-lg-0">
 
